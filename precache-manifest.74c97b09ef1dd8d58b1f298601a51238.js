3568a3f0df15bbd8ad3a5a28ad865a2db12b6454
Lightsource-Pris <israelakinakinsanya@gmail.com>
2020-09-10 11:24:30 +0100

    Updates

---
+++ precache-manifest.74c97b09ef1dd8d58b1f298601a51238.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0ea78280217b3e32bfed9f20fa83b0",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "00be01f946250ac396bc",
+    "url": "/todo/static/js/2.d123c85f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.d123c85f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17aa9ae1a6eb66a31836",
+    "url": "/todo/static/js/main.4e9c479b.chunk.js"
+  },
+  {
+    "revision": "dee5bc951543e248d153",
+    "url": "/todo/static/js/runtime-main.808e74aa.js"
+  }
+]);
